5690aa7b94915190b86efeb4efd41b29710aed42
surekhad96 <surekhasugan96@gmail.com>
2017-06-06 14:38:18 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,temp;
+clrscr();
+scanf("%d%d",&a,&b);
+printf("Before swapping\nx=%d\ny=%d);
+temp=x;
+x=y;
+y=temp;
+printf("After swapping\nx=%d\ny=%d);
+return 0;
+}
